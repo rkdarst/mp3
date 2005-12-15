9336040941110e13c359531f96073b5ab78f4490
rkd <rkd@zgib.net>
2005-12-15 19:42:30 +0200

    add jokes module
    darcs-hash:20051215174230-51e1e-ca4ad0d9bc07e43d5505aab8e0731adf2276581e.gz

--- src/mp3/jokes.py
+++
@@ -1,9 +0,0 @@
-# Richard Darst, 2005
-
-# This is just for fun
-
-jokes = ["Man, they must be getting the OOV, other office virus. \nLet's call our next software package that...",
-         "something goes here"]
-
-import random
-print random.choice(jokes)
