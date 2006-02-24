68514940c5371471c3cc32cec1b30064db4a5e60
rkd <rkd@zgib.net>
2006-02-24 03:46:31 +0200

    add mp3.jokes
    you can do "import mp3.jokes".  But the database is empty right now,
    send more when you think of them.
    
    darcs-hash:20060224014631-51e1e-75e833ab079408ddd220f5a64e6e106966b9714d.gz

---
+++ src/mp3/jokes.py
@@ -0,0 +1,9 @@
+# Richard Darst, 2005
+
+# This is just for fun
+
+jokes = ["Man, they must be getting the OOV, other office virus. \nLet's call our next software package that...",
+         "something goes here"]
+
+import random
+print random.choice(jokes)
