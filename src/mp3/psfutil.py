9908974df18af1059452767c0f89d6093c9ec5f0
jrdowdle <jrdowdle@gmail.com>
2007-04-25 01:48:03 +0300

    psfutil additions:
     - Several functions were added to psfutils in order to facilitate
       converting the topology data in a .psf file (compatible with NAMD) to
       a .top file (compatible with CM3D).  Functions were added to find the
       number of bonds, thetas, dihedrals, and impropers.  A function was
       also added to construct an atomlist containing all of the infor-
       mation in the atom section of the .pdb.  It returns a list of
       dictionaries with said information.  Also, functions were added to
       return lists of bonded pairs of atoms, angles, and torsions.
    
    darcs-hash:20070424224803-7c2ee-617e0abb01e3e57dac0254bdbb2c014ffb4ed964.gz

--- src/mp3/psfutil.py
+++ src/mp3/psfutil.py
@@ -52,6 +52,140 @@ def getnatoms(filename):
         natoms = int(line.split()[0])
         return natoms
 
+
+def getnbonds(filename):
+    """Return the number of bonds in a PSF
+
+    If '!NBOND' is not found in the file, return None and
+    give up.
+    """
+    i = 0
+    for i, line in enumerate(file(filename)):
+        if line.find("!NBOND") == -1:
+            continue
+        nbonds = int(line.split()[0])
+    if nbonds == -1:
+        print 'Could not find NBOND in the file.  is this a PSF?'
+        return None
+    return nbonds
+
+
+def getnthetas(filename):
+    """Return the number of thetas (angles defined by atom triples) in a PSF
+
+    If '!NTHETA' is not found in the file, return None and
+    give up.
+    """    
+    i = 0
+    nthetas = -1
+    for i, line in enumerate(file(filename)):
+        if line.find("!NTHETA") == -1:
+            continue
+        nthetas = int(line.split()[0])
+    if nthetas == -1:
+        print 'Could not find NTHETA in the file.  is this a PSF?'
+        return None
+    return nthetas
+
+
+def getnphis(filename):
+    """Return the number of phis (dihedral angles) in a PSF
+
+    If '!NPHI' is not found in the file, return None and
+    give up.
+    """    
+    i = 0
+    nphis = -1
+    for i, line in enumerate(file(filename)):
+        if line.find("!NPHI") == -1:
+            continue
+        nphis = int(line.split()[0])
+    if nphis == -1:
+        print 'Could not find NPHI in the file.  is this a PSF?'
+        return None
+
+    return nphis
+
+
+def getnimphis(filename):
+    """Return the number of imphis (improper angles) in a PSF
+
+    If '!NIMPHI' is not found in the file, return None and
+    give up.
+    """    
+    i = 0
+    nimphis = -1
+    for i, line in enumerate(file(filename)):
+        if line.find("!NIMPHI") == -1:
+            continue
+        nimphis = int(line.split()[0])
+    if nimphis == -1:
+        print 'Could not find NIMPHI in the file.  is this a PSF?'
+        return None
+
+    return nimphis
+
+
+def getatomlist(filename, nprotatoms=0):
+    """Returns a list of dictionaries of all the atoms in the .psf file and containing all
+    of the .psf information in the atom section.  Can write to the Atom Defs section of
+    a .top file"""
+    atomlist = []
+    inFile = file(filename)
+    for line in inFile:
+        if line.find('!NATOM') == -1:
+            continue
+        if nprotatoms == 0:
+            nprotatoms = int(line.split()[0])        
+        break
+
+    i = 0
+    for line in inFile:
+        if line.strip() == "":
+            break
+        if i >= nprotatoms:
+            break
+        atomlist.append({})
+        atomindex = line.split()[0]
+        resgroup = line.split()[3]
+        atomtype = line.split()[5]
+        charge = line.split()[6]
+        mass = line.split()[7]
+        atomlist[i]['atomindex'] = atomindex
+        atomlist[i]['resgroup'] = resgroup
+        atomlist[i]['atomtype'] = atomtype
+        atomlist[i]['charge'] = charge
+        atomlist[i]['mass'] = mass
+        i += 1
+
+
+    return atomlist
+
+
+def getbondlist(filename):
+    """Returns a tuple that contains the number of bonds as one
+    element of the tuple and a list of bonded pairs of atoms as the
+    other element."""
+    
+    bondlist = []        
+    inFile = file(filename)
+    for line in inFile:
+        if line.find('!NBOND') == -1:
+            continue
+        nbonds = int(line.split()[0])
+        break
+
+    # the .psf file lists four pairs of atoms per line
+    for line in inFile:
+        if line.strip() == "":
+            break
+        for i in xrange(len(line.split())/2):
+            bondlist.append((line.split()[2*i],line.split()[2*i+1]))
+    
+    return nbonds,bondlist
+
+
+
 def getbondconnections(filename, number=None, excludeoutside=True, atomnumberoffset=0):
     """Return the list of connections via bonds in a protein.
     """
@@ -98,6 +232,30 @@ def getbondconnections(filename, number=None, excludeoutside=True, atomnumberoff
 
     return {"n": nbonds,
             "connections": bonds }
+
+
+def getthetalist(filename):
+    """Returns a list of triples defining and theta.
+    """
+    thetalist = []        
+    inFile = file(filename)
+    for line in inFile:
+        if line.find('!NTHETA') == -1:
+            continue
+        ntheta = int(line.split()[0])
+        break
+
+    for line in inFile:
+        if line.strip() == "":
+            break
+        
+        # the .psf file lists three triples of atoms per line        
+        for i in xrange(len(line.split())/3):      
+            thetalist.append((line.split()[3*i],line.split()[3*i+1],line.split()[3*i+2]))
+
+    inFile.close()
+    return ntheta,thetalist
+
 
 
 def getangleconnections(filename, number=None, excludeoutside=True, atomnumberoffset=0):
@@ -155,6 +313,30 @@ def getangleconnections(filename, number=None, excludeoutside=True, atomnumberof
 
     return {"n": nangles,
             "connections": connections }
+
+
+def getdihedrallist(filename):
+    """Returns a list of dihedral forming atoms."""
+    dihedrallist = []        
+    inFile = file(filename)
+    for line in inFile:
+        if line.find('!NPHI') == -1:
+            continue
+        nphi = int(line.split()[0])
+        break
+
+    for line in inFile:
+        if line.strip() == "":
+            break
+        
+        # the .psf file lists two quadruples of atoms per line        
+        for i in xrange(len(line.split())/4):      
+            dihedrallist.append((line.split()[4*i],line.split()[4*i+1],line.split()[4*i+2],
+                                 line.split()[4*i+3]))
+
+    inFile.close()
+    return nphi,dihedrallist
+
 
 
 def getdihedralconnections(filename, number=None, excludeoutside=True, atomnumberoffset=0):
@@ -228,6 +410,31 @@ def getdihedralconnections(filename, number=None, excludeoutside=True, atomnumbe
 
     return {"n": ndihedrals,
             "connections": connections }
+
+
+def getimproperlist(filename):
+    """Returns a list of improper forming atoms."""
+    improperlist = []        
+    inFile = file(filename)
+    for line in inFile:
+        if line.find('!NIMPHI') == -1:
+            continue
+        nimphi = int(line.split()[0])
+        break
+
+    for line in inFile:
+        if line.strip() == "":
+            break
+        
+        # the .psf file lists two quadruples of atoms per line        
+        for i in xrange(len(line.split())/4):      
+            improperlist.append((line.split()[4*i],line.split()[4*i+1],line.split()[4*i+2],
+                                 line.split()[4*i+3]))
+
+    inFile.close()
+    return nimphi,improperlist
+
+
 
 
 def getimproperconnections(filename, number=None, excludeoutside=True, atomnumberoffset=0):
@@ -324,25 +531,74 @@ def commonconnections(*lists):
 
 
 if __name__ == "__main__":
-    filename = "files/apomb2C_SPCEions.psf"
-    print getnatoms(filename)
-    bondconn = getbondconnections(filename,
-                             number=2464,
-                             excludeoutside=True,
-                             atomnumberoffset=-1)
-    angleconn = getangleconnections(filename,
-                              number=2464,
-                              excludeoutside=True,
-                              atomnumberoffset=-1)
-    dihedralconn = getdihedralconnections(filename,
+    if False:
+        filename = "files/apomb2C_SPCEions.psf"
+        print getnatoms(filename)
+        bondconn = getbondconnections(filename,
                                  number=2464,
                                  excludeoutside=True,
                                  atomnumberoffset=-1)
-    #print bondconn
-    #print angleconn
-    #print dihedralconn
+        angleconn = getangleconnections(filename,
+                                  number=2464,
+                                  excludeoutside=True,
+                                  atomnumberoffset=-1)
+        dihedralconn = getdihedralconnections(filename,
+                                     number=2464,
+                                     excludeoutside=True,
+                                     atomnumberoffset=-1)
+        #print bondconn
+        #print angleconn
+        #print dihedralconn
 
-    print commonconnections(bondconn["connections"],
-                            angleconn["connections"],
-                            dihedralconn["connections"])
-    
+        print commonconnections(bondconn["connections"],
+                                angleconn["connections"],
+                                dihedralconn["connections"])
+
+    if True:
+##         Tests added by John R. Dowdle, 04/24/2007 to test the
+##         getatomlist, getbondlist, getthetalist, getdihedrallist,
+##         and getimproperlist functions.
+        filename = 'files/ubq_patched.psf'
+        atomlist = getatomlist(filename,nprotatoms=1231)
+        print atomlist
+        nbonds,bondlist = getbondlist(filename)
+        print 'nbonds = ',nbonds
+        print 'len(bondlist) = ',len(bondlist)
+        print 'first five bonds:  '
+        print bondlist[0:5]
+        print 'last five bonds:  '
+        print bondlist[len(bondlist)-5:len(bondlist)]        
+        
+        nthetas,thetalist = getthetalist(filename)
+        print 'nthetas = ',nthetas
+        print 'len(thetalist) = ',len(thetalist)
+        print 'first five thetas:  '
+        print thetalist[0:5]
+        print 'last five thetas:  '
+        print thetalist[len(thetalist)-5:len(thetalist)]        
+        
+        ndihedrals,dihedrallist = getdihedrallist(filename)
+        print 'ndihedrals = ',ndihedrals
+        print 'len(dihedrallist) = ',len(dihedrallist)
+        print 'first five dihedrals:  '
+        print dihedrallist[0:5]
+        print 'last five dihedrals:  '
+        print dihedrallist[len(dihedrallist)-5:len(dihedrallist)]        
+        
+        nimpropers,improperlist = getimproperlist(filename)
+        print 'nimpropers = ',nimpropers
+        print 'len(improperlist) = ',len(improperlist)
+        print 'first five impropers:  '
+        print improperlist[0:5]
+        print 'last five impropers:  '
+        print improperlist[len(improperlist)-5:len(improperlist)]        
+        
+        nbonds = getnbonds(filename)
+        print 'nbonds = ',nbonds
+        nthetas = getnthetas(filename)
+        print 'nthetas = ',nthetas
+        nphis = getnphis(filename)
+        print 'nphis = ',nphis
+        nimphis = getnimphis(filename)
+        print 'nimphis = ',nimphis
+        
